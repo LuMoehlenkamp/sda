3c06be354e58951d26486f96c385ba34aa887a20
LuMoehlenkamp <36656660+LuMoehlenkamp@users.noreply.github.com>
2022-04-21 09:54:06 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ludger/source/sda
-# Build directory: /home/ludger/source/sda/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")

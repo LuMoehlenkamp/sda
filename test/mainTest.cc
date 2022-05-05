f1f0da1450f64cd1c0e1f205cc095cb4b93fa68d
ludger_m <ludger.moehlenkamp@googlemail.com>
2022-05-05 20:34:51 +0200

    rearranged cmake file to allow unit testing

--- test/mainTest.cc
+++ test/mainTest.cc
@@ -1,7 +1,5 @@
-#define BOOST_TEST_MAIN
-#if !defined( WIN32 )
-    #define BOOST_TEST_DYN_LINK
-#endif
+// #define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE test_senecDataAcquisition
 
 #include <boost/test/unit_test.hpp>
 

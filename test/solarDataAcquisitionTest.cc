a66f4eb899fedb9b68523b019ce157d92b26a8b3
ludger_m <ludger.moehlenkamp@googlemail.com>
2022-06-14 22:58:02 +0200

    added unit test for solarDataAcquisition class

--- test/solarDataAcquisitionTest.cc
+++ test/solarDataAcquisitionTest.cc
@@ -1,12 +1,12 @@
 #include <boost/test/unit_test.hpp>
-#include "../src/senecDataAcquisition.hh"
+#include "../src/solarDataAcquisition.hh"
 
-BOOST_AUTO_TEST_SUITE(SenecDataAcquisitionTest)
+BOOST_AUTO_TEST_SUITE(SolarDataAcquisitionTest)
 
 BOOST_AUTO_TEST_CASE(first_and_only_test_case)
 {
   BOOST_TEST_MESSAGE("starting unit tests\n");
-  SDA::SenecDataAcquisition sda;
+  SDA::SolarDataAcquisition sda;
   int result = sda();
   BOOST_REQUIRE(result == 0);
   BOOST_TEST_MESSAGE("unit testing finished\n");

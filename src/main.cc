85d0c7bb015d8f156029fb1d5f5ee10e33331da8
ludger_m <ludger.moehlenkamp@googlemail.com>
2022-10-11 01:04:23 +0200

    modified main to call only senecDataAcquisition

--- src/main.cc
+++ src/main.cc
@@ -15,12 +15,10 @@ int main(int argc, char *argv[])
   boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work =
     boost::asio::make_work_guard(ioContext);
 
-  SDA::SenecDataAcquisition sda(ioContext, 15U);
+  SDA::SenecDataAcquisition senec_da(ioContext, 15U);
+  // SDA::SolarDataAcquisition solar_da(ioContext, 10U);
 
   ioContext.run();
   
-  SDA::SolarDataAcquisition solar_da;
-  solar_da();
-
   return 0;
 }
